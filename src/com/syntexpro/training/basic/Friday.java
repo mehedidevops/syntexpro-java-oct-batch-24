b7c4d31713811cf8e0ddefe386bb4093ff49b7e4
Mehedi Aziz <mehediqa@yahoo.com>
2024-10-12 11:11:01 -0400

    friday

---
+++ src/com/syntexpro/training/basic/Friday.java
@@ -0,0 +1,10 @@
+package com.syntexpro.training.basic;
+
+public class Friday {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello my name is Mehedi, I am a senior java engineer");
+
+    }
+}
